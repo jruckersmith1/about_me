e2bd2d0ba0a25a091ac9a9a424b43b7c83aa89a8
Janet Rucker-Smith <jruckersmith1@yahoo.com>
2017-08-03 16:25:48 -0700

    Day 4 lab

--- script.js
+++ script.js
@@ -1,52 +1,74 @@
-alert( 'Greetings!' );
-var alias = prompt( 'what is your name?' );
-console.log( 'hello!' );
+function aliens () {
 
-alert( 'hi there ' + alias + '! Nice to meet you' );
-var warpSpeed = confirm( 'Are you cabale of warp speed?');
-if ( warpSpeed ) {
+  alert( 'Greetings!' );
+  var alias = prompt( 'what is your name?' );
+  console.log( 'hello!' );
 
-  var warpSpeed = prompt ( 'Do you like flying saucers or WAR BIRDS?').toUpperCase();
-} else {
-  var warpSpeed = prompt ( 'Oh, that\'s too bad! Do you like flying saucers or WAR BIRDS?').toUpperCase();
-}
-//'war birds' === 'WAR BIRDS'
-if ( warpSpeed === 'WAR BIRDS' )
+  alert( 'hi there ' + alias + '! Nice to meet you' );
+  var warpSpeed = confirm( 'Are you cabale of warp speed?');
+  if ( warpSpeed ) {
 
-  var homePlanet = confirm( 'Are you from the Klingon Empire?' );
-
-if ( homePlanet ) {
-  var favSpecies = prompt( 'What species is your favorite?' ).toLowerCase();
-
-  // 'Vulcan' === 'vulcan'
-  if ( favSpecies === 'vulcan' ) {
-    alert( 'mine, too!' ); 
+    var warpSpeed = prompt ( 'Do you like flying saucers or WAR BIRDS?').toUpperCase();
   } else {
-    alert( favSpecies + ' are you serious?!' );
+    var warpSpeed = prompt ( 'Oh, that\'s too bad! Do you like flying saucers or WAR BIRDS?').toUpperCase();
+  }
+  //'war birds' === 'WAR BIRDS'
+  if ( warpSpeed === 'WAR BIRDS' )
+
+    var homePlanet = confirm( 'Are you from the Klingon Empire?' );
+
+  if ( homePlanet ) {
+    var favSpecies = prompt( 'What species is your favorite?' ).toLowerCase();
+
+    // 'Vulcan' === 'vulcan'
+    if ( favSpecies === 'vulcan' ) {
+      alert( 'mine, too!' ); 
+    } else {
+      alert( favSpecies + ' are you serious?!' );
+    }
   }
 }
+var score = 0;
+function realTrekker() {
+  var trekChar = [ 'picard', 'spock', 'worf', 'data', ];
+  do {
 
-var trekChar = [ 'picard', 'spock', 'worf', 'data', ];
+    var guess = prompt( 'Which Star Trek character am I thinking about?' ).toLowerCase();
+    if ( trekChar.indexOf ( guess ) !== -1) {
+      alert( 'well done, trekker!');
+      score = score + 1;
+      break;
 
-do {
+    } else {
+      alert( 'Sorry, you\'re not a trekker after all.');
+    }
 
-  var guess = prompt( 'Which Star Trek character am I thinking about?').toLowerCase();
+  } while ( guess !== trekChar );
   console.log( guess, trekChar[0]);
-} while ( guess !== trekChar[0]);
-
-for ( var i = 0; i < 4; i++ ) {
-  var citiesLivedIn = parseInt(prompt ( 'How many cities have I lived in?' ));
-  console.log( ' The numnber of cities lived in is: ' + citiesLivedIn );
-
-  if (citiesLivedIn > 13) {
-    alert( 'too high!');
-  } else if (citiesLivedIn < 13) {
-    alert( 'not enough!');  
-  } else if (citiesLivedIn === 13) {
-    alert( 'exactly! Good job!');
-    break;
-  } else { 
-    alert( 'enter a number' ); 
-  }
-
 }
+
+function cities() {
+
+  for ( var i = 0; i < 4; i++ ) {
+    var citiesLivedIn = parseInt(prompt ( 'How many cities have I lived in?' ));
+    console.log( ' The numnber of cities lived in is: ' + citiesLivedIn );
+
+    if (citiesLivedIn > 13) {
+      alert( 'too high!');
+    } else if (citiesLivedIn < 13) {
+      alert( 'not enough!');  
+    } else if (citiesLivedIn === 13) {
+      alert( 'exactly! Good job!');
+      score = score + 1;
+      break;
+    } else { 
+      alert( 'enter a number' ); 
+    }
+  }
+}
+
+aliens();
+realTrekker();
+cities();
+
+alert( 'Your score is ' + score);
